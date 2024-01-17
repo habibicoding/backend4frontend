bf591640e9729a7b515922daf6b82357f026ce18
Habibi Coding <habibicoding@aol.com>
2024-01-17 20:31:22 +0100

    chore: User version numbers.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,22 +18,22 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    runtimeOnly("org.postgresql:postgresql")
-    runtimeOnly("com.h2database:h2")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.1.0")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.1.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.22")
+    implementation("org.springframework.boot:spring-boot-starter-validation:3.1.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.22")
+    runtimeOnly("org.postgresql:postgresql:42.7.1")
+    runtimeOnly("com.h2database:h2:2.2.224")
 
     // Swagger / Open API
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
 
     // Unit Testing stuff
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.assertj:assertj-core:3.24.2")
-    testImplementation("io.mockk:mockk:1.13.4")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.0")
+    testImplementation("org.assertj:assertj-core:3.25.1")
+    testImplementation("io.mockk:mockk:1.13.9")
 }
 
 tasks.withType<KotlinCompile> {
